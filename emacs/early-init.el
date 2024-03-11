b410b47fa13134f24566c64841543ffba2247d0e
contagnas <contagnas@gmail.com>
2024-03-11 12:12:33 -0400

    emacs config

---
+++ emacs/early-init.el
@@ -0,0 +1,33 @@
+;; This file is generate by readme.org. Do not edit manually!
+
+(setq gc-cons-threshold most-positive-fixnum
+      gc-cons-percentage 1)
+
+(defun +gc-after-focus-change ()
+  "Run GC when frame loses focus."
+  (run-with-idle-timer
+   5 nil
+   (lambda () (unless (frame-focus-state) (garbage-collect)))))
+
+(defun +reset-init-values ()
+  (run-with-idle-timer
+   1 nil
+   (lambda ()
+     (setq ;;file-name-handler-alist default-file-name-handler-alist
+	   gc-cons-percentage 0.1
+	   gc-cons-threshold 100000000)
+     (message "gc-cons-threshold")
+     (when (boundp 'after-focus-change-function)
+       (add-function :after after-focus-change-function #'+gc-after-focus-change)))))
+
+(with-eval-after-load 'elpaca
+  (add-hook 'elpaca-after-init-hook '+reset-init-values))
+
+(setq package-enable-at-startup nil)
+
+(push '(menu-bar-lines . 0) default-frame-alist)
+(push '(tool-bar-lines . 0) default-frame-alist)
+(push '(vertical-scroll-bars) default-frame-alist)
+
+(setq ring-bell-function #'ignore
+      inhibit-startup-screen t)
---
+++ emacs/init.el
@@ -0,0 +1,18 @@
+;;; Commentary:
+;; Emacs init file responsible for either loading a pre-compiled configuration
+;; file or tangling and loading a literate org configuration file.
+
+;; Don't attempt to find/apply special file handlers to files loaded during
+;; startup.
+(let ((file-name-handler-alist nil))
+  ;; If config is pre-compiled, then load that
+  (if (file-exists-p (expand-file-name "readme.elc" user-emacs-directory))
+      (load-file (expand-file-name "readme.elc" user-emacs-directory))
+    ;; Otherwise use org-babel to tangle and load the configuration
+    (require 'org)
+
+    ;; This might not work exactly as expected - readme.org also contains early-init.el code,
+    ;; which will have already been executed. Oh well?
+    (org-babel-load-file (expand-file-name "readme.org" user-emacs-directory))))
+
+;;; init.el ends here
