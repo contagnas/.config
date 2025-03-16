846e5380d07b036cc4317c835aaa6b8850c03dcb
contagnas <contagnas@gmail.com>
2025-03-16 10:57:27 -0700

    correct emacs binding

--- nixos/home-manager/home.nix
+++ nixos/home-manager/home.nix
@@ -5,7 +5,7 @@
   config,
   pkgs,
   ...
-}: {
+}: rec {
   imports = [];
 
   home = {
@@ -39,12 +39,18 @@
 
   programs.home-manager.enable = true;
   programs.alacritty.enable = true;
-  programs.emacs.enable = true;
+  programs.emacs = {
+    enable = true;
+    # pgtk = pure gtk, has wayland support
+    package = pkgs.emacs-pgtk;
+  };
   programs.git.enable = true;
   programs.fuzzel.enable = true;
+  programs.fuzzel.settings = {
+    border.radius = "0";
+  };
 
   stylix.targets.niri.enable = true;
-  stylix.targets.waybar.addCss = true;
 
   stylix.fonts = {
     sansSerif = config.stylix.fonts.monospace;
@@ -52,7 +58,7 @@
   };
 
   programs.niri = {
-    settings = {
+    settings = let xwaylandPort = ":69"; in {
       binds = with config.lib.niri.actions; {
         "Mod+Shift+E".action = quit;
         "Mod+Q".action = close-window;
@@ -62,11 +68,13 @@
         "Mod+K".action = focus-window-or-workspace-up;
         "Mod+L".action = focus-column-right;
 
-
         "Mod+Shift+H".action = move-column-left;
-        "Mod+Shift+J".action = move-window-down;
-        "Mod+Shift+K".action = move-window-up;
+        "Mod+Shift+J".action = move-window-down-or-to-workspace-down;
+        "Mod+Shift+K".action = move-window-up-or-to-workspace-up;
         "Mod+Shift+L".action = move-column-right;
+
+        "Mod+Alt+H".action = set-column-width "-5%";
+        "Mod+Alt+L".action = set-column-width "+5%";
 
         "Mod+F".action = switch-preset-column-width;
         "Mod+Shift+F".action = maximize-column;
@@ -74,10 +82,16 @@
 
         "Mod+WheelScrollUp".action = focus-workspace-up;
         "Mod+WheelScrollDown".action = focus-workspace-down;
+        "Mod+Shift+WheelScrollUp".action = focus-column-left;
+        "Mod+Shift+WheelScrollDown".action = focus-column-right;
 
         "Mod+R".action.spawn = "${pkgs.fuzzel}/bin/fuzzel";
         "Mod+G".action.spawn = "${pkgs.google-chrome}/bin/google-chrome-stable";
+        # "Mod+E".action.spawn = "${pkgs.emacs}/bin/emacs"; # need to use emacs-with-packages, not base emacs
+        "Mod+E".action.spawn = "${config.programs.emacs.finalPackage}/bin/emacs"; # need to use emacs-with-packages, not base emacs
         "Mod+T".action.spawn = "${pkgs.alacritty}/bin/alacritty";
+
+        "Mod+V".action = toggle-window-floating;
       };
 
       outputs."DP-1" = {
@@ -95,6 +109,14 @@
         };
 
         mouse.accel-profile = "flat";
+
+        focus-follows-mouse = {
+            enable = true;
+            max-scroll-amount = "25%";
+        };
+
+        warp-mouse-to-focus = true;
+
       };
 
       prefer-no-csd = true;
@@ -108,17 +130,23 @@
           { proportion = 0.50; }
         ];
 
-        default-column-width = { proportion = 0.25; };
+        default-column-width = { proportion =0.25; };
+
+        focus-ring = {
+          width = 10000;
+          active.color = "#00000055";
+        };
       };
 
       animations.slowdown = 0.5;
 
       spawn-at-startup = [
-        { command = ["${pkgs.xwayland-satellite}/bin/xwayland-satellite"]; }
+        { command = ["${pkgs.xwayland-satellite}/bin/xwayland-satellite" xwaylandPort]; }
+        { command = ["${pkgs.mako}/bin/mako"]; }
       ];
 
       environment = {
-        DISPLAY = ":0";
+        DISPLAY = xwaylandPort;
         OZONE_PLATFORM = "wayland";
         NIXOS_OZONE_WL = "1";
         MOZ_ENABLE_WAYLAND = "1";
